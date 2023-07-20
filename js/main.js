79a56a6800f067bb670353cfe838ab3bc6d4b20b
ri <138934791+peanuttbutter@users.noreply.github.com>
2023-07-20 22:04:24 +0300

    fixed mobile nav

--- js/main.js
+++ js/main.js
@@ -2,12 +2,21 @@
 const navBtn = document.querySelector('.nav-icon-btn');
 const navIcon = document.querySelector('.nav-icon');
 const nav = document.querySelector('.header__top-row')
+const navList = document.querySelector('.nav__list')
 
 
 navBtn.onclick = function () {
-    navIcon.classList.toggle('nav-icon--active');
-    nav.classList.toggle('header__top-row--mobile');
-    document.body.classList.toggle('no-scroll')
+    mobileNav();
+}
+
+navList.onclick = function () {
+	mobileNav();
+}
+
+function mobileNav () {
+	nav.classList.toggle('header__top-row--mobile');
+	navIcon.classList.toggle('nav-icon--active');
+	document.body.classList.toggle('no-scroll');
 }
 
 /* Phone Mask */
